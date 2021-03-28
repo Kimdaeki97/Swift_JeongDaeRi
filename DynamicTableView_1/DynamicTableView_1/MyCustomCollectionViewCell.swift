6c4e7033691b371e64e0c77b8fa8dd57dc76419e
Kimdaeki97 <daeki56@gmail.com>
2021-03-28 22:59:43 +0900

    콜렉션 뷰 셀

--- DynamicTableView_1/DynamicTableView_1/MyCustomCollectionViewCell.swift
+++ DynamicTableView_1/DynamicTableView_1/MyCustomCollectionViewCell.swift
@@ -34,3 +34,4 @@ class MyCustomCollectionViewCell: UICollectionViewCell {
         self.contentView.layer.borderColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
     }
 }
+
